b49b9d13e33555f7a709cde9131dcb86a7416177
Brandon Benefield <bsquared18@gmail.com>
2018-03-23 18:13:54 -0400

    Add mobile nav links cascading effect on dropdown

--- js/main.js
+++ js/main.js
@@ -5,27 +5,34 @@ body.addEventListener('click', e => {
     const navbarLinksContainer = document.querySelector('.navbar__links-container');
     const navbarLinksItem = document.querySelectorAll('.navbar__links-container__item');
     
+    const sleep = (ms) => {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    };
+    
+    const foo = async () => {
+      let count = 50;
+      
+      for (let item of navbarLinksItem) {
+        await sleep(count);
+        
+        if (navbarLinksContainer.classList.contains('navbar__links-container--open')) {
+          item.classList.add('navbar__links-container__item--show');
+        }
+        else {
+          item.classList.remove('navbar__links-container__item--show');
+        }
+        
+        count += 75;
+      }
+    }; 
     
     if (navbarLinksContainer.classList.contains('navbar__links-container--open')) {
       navbarLinksContainer.classList.remove('navbar__links-container--open');
-      
-      for (let item of navbarLinksItem) {
-        // item.classList.remove('navbar__links-container__item--show');
-        item.style.opacity = '0';
-      }
     }
     else {
-      let count = .5;
-      
       navbarLinksContainer.classList.add('navbar__links-container--open');
-      
-      for (let item of navbarLinksItem) {
-        // item.classList.add('navbar__links-container__item--show');
-        item.style.transition = `${ count }s`;
-        item.style.opacity = '1';
-        
-        count += .2;
-      }
     }
+    
+    foo();
   }
 });
